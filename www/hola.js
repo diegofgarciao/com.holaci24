d1895b12f02275e51af43a049bafdbfae35f34b6
diegofgarciao <diegofgarciao@users.noreply.github.com>
2017-01-23 11:24:15 -0500

    Create hola.js

---
+++ www/hola.js
@@ -0,0 +1,13 @@
+
+
+module exports = (function(){
+
+    var mostrar = function(step, successCallback, errorCallback){
+    cordova.exec(successCallback, errorCallback, "holaFuncion", "holaMundo", [step]);};
+    
+    return {
+    callInit:mostrar;
+    };
+    
+
+})();
